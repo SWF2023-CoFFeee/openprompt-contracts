67c3c6ab11e1f0a2acae9d10ba160e2ff41df663
jiheon788 <jiheon788@ajou.ac.kr>
2023-08-01 02:20:34 +0900

    chore: npm 스크립트 등록

--- Contract/hardhat.config.ts
+++ Contract/hardhat.config.ts
@@ -6,21 +6,20 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.17',
+        version: "0.8.17",
         settings: {
-          optimizer:
-          {
+          optimizer: {
             enabled: true,
             runs: 200,
           },
-        }
+        },
       },
-    ]
+    ],
   },
   networks: {
     ganache: {
-      url: 'http://127.0.0.1:8545',
-      chainId: 1337,
+      url: "http://127.0.0.1:8545",
+      chainId: 7722,
     },
   },
 };
